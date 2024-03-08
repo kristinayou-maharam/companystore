5c87c17d3e53e1c139bec0837bb970a09f28f22a
kristinayou-maharam <141076633+kristinayou-maharam@users.noreply.github.com>
2024-03-08 13:08:21 -0500

    Changes to modal

--- companystore-v9/script.js
+++ companystore-v9/script.js
@@ -17,12 +17,12 @@ document.addEventListener("DOMContentLoaded", function () {
         <p> ${stationery.description}</p>
         <p>Price: $${stationery.price.toFixed(2)}</p>
         <p>Quantity per Order: ${stationery.quantity}</p>
-        <label for="variationDropdown"><p>Variation:</p></label>
+        <label for="variationDropdown"><p style="display:inline-block">Variation:</p></label>
         <select id="variationDropdown">${dropdownOptions}</select><br><br>
-        <p id="don">Order: </p><p id="selectedValue"></p>
-        <p id=note>${stationery.Note}</p>
+        <p id="don">Item Code: </p><p id="selectedValue"></p>
+        <p id=note>Order System: ${stationery.Note}</p>
         <p style="display: inline-block">Quantity:</p><input type="number" id="quantity" name="quantity" min="1" max="5" value="1">
-        <button id= "addtocart"><h5>Add to Cart</h5></button>
+        <button id= "addtocart"><h5>Add to Wishlist</h5></button>
 
             </div>
         `;
@@ -82,12 +82,12 @@ document.addEventListener("DOMContentLoaded", function () {
         <p> ${neww.description}</p>
         <p>Price: $${neww.price.toFixed(2)}</p>
         <p>Quantity per Order: ${neww.quantity}</p>
-        <label for="variationDropdown"><p>Variation:</p></label>
+        <label for="variationDropdown"><p style="display:inline-block">Variation:</p></label>
         <select id="variationDropdown">${dropdownOptions}</select><br><br>
-        <p id="don">Order: </p><p id="selectedValue"></p>
-        <p id=note>${neww.Note}</p>
+        <p id="don">Item Code: </p><p id="selectedValue"></p>
+        <p id=note>Order System: ${neww.Note}</p>
         <p style="display: inline-block">Quantity:</p><input type="number" id="quantity" name="quantity" min="1" max="5" value="1">
-        <button id= "addtocart"><h5>Add to Cart</h5></button>
+        <button id= "addtocart"><h5>Add to Wishlist</h5></button>
             </div>
         `;
         document.getElementById("product-info").innerHTML = newInfoHTML;
@@ -149,12 +149,12 @@ document.addEventListener("DOMContentLoaded", function () {
         <p> ${test.description}</p>
         <p>Price: $${test.price.toFixed(2)}</p>
         <p>Quantity per Order: ${test.quantity}</p>
-        <label for="variationDropdown"><p>Variation:</p></label>
+        <label for="variationDropdown"><p style="display:inline-block">Variation:</p></label>
         <select id="variationDropdown">${dropdownOptions}</select><br><br>
-        <p id="don">Order: </p><p id="selectedValue"></p>
-        <p id=note>${test.Note}</p>
+        <p id="don">Item Code: </p><p id="selectedValue"></p>
+        <p id=note>Order System: ${test.Note}</p>
         <p style="display: inline-block">Quantity:</p><input type="number" id="quantity" name="quantity" min="1" max="5" value="1">
-        <button id= "addtocart"><h5>Add to Cart</h5></button>
+        <button id= "addtocart"><h5>Add to Wishlist</h5></button>
 
             </div>
         `;
@@ -209,7 +209,7 @@ document.addEventListener("DOMContentLoaded", function () {
             <p>Order: ${books.DON_reference_number}</p>
             <p>${books.Note}</p>
             <p style="display: inline-block">Quantity:</p><input type="number" id="quantity" name="quantity" min="1" max="5" value="1">
-            <button id= "addtocart"><h5>Add to Cart</h5></button>
+            <button id= "addtocart"><h5>Add to Wishlist</h5></button>
 
             </div>
         `;
@@ -235,12 +235,12 @@ document.addEventListener("DOMContentLoaded", function () {
         <p> ${books.description}</p>
         <p>Price: $${books.price.toFixed(2)}</p>
         <p>Quantity per Order: ${books.quantity}</p>
-        <label for="variationDropdown"><p>Variation:</p></label>
+        <label for="variationDropdown"><p style="display:inline-block">Variation:</p></label>
         <select id="variationDropdown">${dropdownOptions}</select><br><br>
-        <p id="don">Order: </p><p id="selectedValue"></p>
-        <p id=note>${books.Note}</p>
+        <p id="don">Item Code: </p><p id="selectedValue"></p>
+        <p id=note>Order System: ${books.Note}</p>
         <p style="display: inline-block">Quantity:</p><input type="number" id="quantity" name="quantity" min="1" max="5" value="1">
-        <button id= "addtocart"><h5>Add to Cart</h5></button>
+        <button id= "addtocart"><h5>Add to Wishlist</h5></button>
 
             </div>
         `;
@@ -301,12 +301,12 @@ document.addEventListener("DOMContentLoaded", function () {
         <p> ${print.description}</p>
         <p>Price: $${print.price.toFixed(2)}</p>
         <p>Quantity per Order: ${print.quantity}</p>
-        <label for="variationDropdown"><p>Variation:</p></label>
+        <label for="variationDropdown"><p style="display:inline-block">Variation:</p></label>
         <select id="variationDropdown">${dropdownOptions}</select><br><br>
-        <p id="don">Order: </p><p id="selectedValue"></p>
-        <p id=note>${print.Note}</p>
+        <p id="don">Item Code: </p><p id="selectedValue"></p>
+        <p id=note>Order System: ${print.Note}</p>
         <p style="display: inline-block">Quantity:</p><input type="number" id="quantity" name="quantity" min="1" max="5" value="1">
-        <button id= "addtocart"><h5>Add to Cart</h5></button>
+        <button id= "addtocart"><h5>Add to Wishlist</h5></button>
 
             </div>
         `;
@@ -368,12 +368,12 @@ document.addEventListener("DOMContentLoaded", function () {
         <p> ${presentation.description}</p>
         <p>Price: $${presentation.price.toFixed(2)}</p>
         <p>Quantity per Order: ${presentation.quantity}</p>
-        <label for="variationDropdown"><p>Variation:</p></label>
+        <label for="variationDropdown"><p style="display:inline-block">Variation:</p></label>
         <select id="variationDropdown">${dropdownOptions}</select><br><br>
-        <p id="don">Order: </p><p id="selectedValue"></p>
-        <p id=note>${presentation.Note}</p>
+        <p id="don">Item Code: </p><p id="selectedValue"></p>
+        <p id=note>Order System: ${presentation.Note}</p>
         <p style="display: inline-block">Quantity:</p><input type="number" id="quantity" name="quantity" min="1" max="5" value="1">
-        <button id= "addtocart"><h5>Add to Cart</h5></button>
+        <button id= "addtocart"><h5>Add to Wishlist</h5></button>
 
             </div>
         `;
@@ -434,12 +434,12 @@ document.addEventListener("DOMContentLoaded", function () {
         <p> ${sampling.description}</p>
         <p>Price: $${sampling.price.toFixed(2)}</p>
         <p>Quantity per Order: ${sampling.quantity}</p>
-        <label for="variationDropdown"><p>Variation:</p></label>
+        <label for="variationDropdown"><p style="display:inline-block">Variation:</p></label>
         <select id="variationDropdown">${dropdownOptions}</select><br><br>
-        <p id="don">Order: </p><p id="selectedValue"></p>
-        <p id=note>${sampling.Note}</p>
+        <p id="don">Item Code: </p><p id="selectedValue"></p>
+        <p id=note>Order System: ${sampling.Note}</p>
         <p style="display: inline-block">Quantity:</p><input type="number" id="quantity" name="quantity" min="1" max="5" value="1">
-        <button id= "addtocart"><h5>Add to Cart</h5></button>
+        <button id= "addtocart"><h5>Add to Wishlist</h5></button>
 
             </div>
         `;
@@ -482,6 +482,94 @@ document.addEventListener("DOMContentLoaded", function () {
       };
     })
 
+
+    // document.getElementById("searchquerysubmit").addEventListener("click", function () {
+    //   const searchQuery = document.getElementById("searchqueryinput").value;
+    //   fetch("./src/new.json")
+    //     .then(response => response.json())
+    //     .then(data => {
+    //       const matchingNew = data.filter(item => item.name.includes(searchQuery));
+    //       if (matchingNew.length > 0) {
+    //         window.displayNew(matchingNew[0].index);
+    //       } else {
+    //         console.log("No matching stationery found.");
+    //       }
+    //     })
+    //     .catch(error => {
+    //       console.error("Error fetching stationery data:", error);
+    //     });
+    // document.getElementById("searchquerysubmit").addEventListener("click", function () {
+    //   const searchQuery = document.getElementById("searchqueryinput").value;
+    //   fetch("./src/stationery.json")
+    //     .then(response => response.json())
+    //     .then(data => {
+    //       const matchingStationery = data.filter(item => item.name.includes(searchQuery));
+    //       if (matchingStationery.length > 0) {
+    //         window.displayStationery(matchingStationery[0].index);
+    //       } else {
+    //         console.log("No matching stationery found.");
+    //       }
+    //     })
+    //     .catch(error => {
+    //       console.error("Error fetching stationery data:", error);
+    //     });
+
+    //   fetch("./src/books.json")
+    //     .then(response => response.json())
+    //     .then(data => {
+    //       const matchingBooks = data.books.filter(book => book.name.includes(searchQuery));
+    //       if (matchingBooks.length > 0) {
+    //         window.displayBooks(matchingBooks[0].index);
+    //       } else {
+    //         console.log("No matching books found.");
+    //       }
+    //     })
+    //     .catch(error => {
+    //       console.error("Error fetching books data:", error);
+    //     });
+
+    //   fetch("./src/print.json")
+    //     .then(response => response.json())
+    //     .then(data => {
+    //       const matchingPrints = data.print.filter(print => print.name.includes(searchQuery));
+    //       if (matchingPrints.length > 0) {
+    //         window.displayPrint(matchingPrints[0].index);
+    //       } else {
+    //         console.log("No matching prints found.");
+    //       }
+    //     })
+    //     .catch(error => {
+    //       console.error("Error fetching print data:", error);
+    //     });
+
+    //   fetch("./src/presentation.json")
+    //     .then(response => response.json())
+    //     .then(data => {
+    //       const matchingPresentations = data.presentation.filter(presentation => presentation.name.includes(searchQuery));
+    //       if (matchingPresentations.length > 0) {
+    //         window.displayPresentation(matchingPresentations[0].index);
+    //       } else {
+    //         console.log("No matching presentations found.");
+    //       }
+    //     })
+    //     .catch(error => {
+    //       console.error("Error fetching presentation data:", error);
+    //     });
+
+    //   fetch("./src/sampling.json")
+    //     .then(response => response.json())
+    //     .then(data => {
+    //       const matchingSamplings = data.sampling.filter(sampling => sampling.name.includes(searchQuery));
+    //       if (matchingSamplings.length > 0) {
+    //         window.displaySampling(matchingSamplings[0].index);
+    //       } else {
+    //         console.log("No matching samplings found.");
+    //       }
+    //     })
+    //     .catch(error => {
+    //       console.error("Error fetching sampling data:", error);
+    //     });
+    // });
 
 
 });
